2e909bf4e0e8bc0b6857832f3ad658127ebb2ed8
Boleslav <bolik.kg@gmail.com>
2023-01-26 15:00:18 +0600

    Решение первой задачи

--- HomeWork_S2/Program.cs
+++ HomeWork_S2/Program.cs
@@ -1,4 +1,4 @@
-﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным?
 // По двум заданным числам проверять является ли одно квадратом другого
 // Задать номер четверти, показать диапазоны для возможных координат
 // Найти расстояние между точками в пространстве 2D/3D
@@ -20,8 +20,6 @@ int Proverca_chisla()
   return num;
 }
 
-
-
 int otvet = 1;
 string otvet2 = " ";
 do
@@ -33,7 +31,7 @@ do
     Console.WriteLine(" ");
     Console.WriteLine("Добрый день прошу выбрать цифру из списка что Вы хотите сделать?");
     Console.WriteLine("__________");
-    Console.WriteLine("1. Дано число обозначающее день недели. Выяснить является номер дня недели выходным");
+    Console.WriteLine("1. Дано число обозначающее день недели. Выяснить является номер дня недели выходным?");
     Console.WriteLine("2. По двум заданным числам проверять является ли одно квадратом другого");
     Console.WriteLine("3. Задать номер четверти, показать диапазоны для возможных координат");
     Console.WriteLine("4. Найти расстояние между точками в пространстве 2D");
@@ -54,37 +52,42 @@ do
 
   Console.Clear();
 
-  //1. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+//Начало тела задач
+  //1. Дано число обозначающее день недели. Выяснить является номер дня недели выходным?
   if (otvet == 1)
     {
       int a1;
-      Console.Write("Введите число: ");
+      Console.Write("Введите число обозначающее день недели: ");
       a1 = Proverca_chisla();
-      Console.WriteLine($"Вы ввели: {a1}");
-    }  
-/*    switch(a1)
-{
-  case 1 : Console.WriteLine("Это день недели Понедельник!"); break;
-  case 2 : Console.WriteLine("Это день недели Вторник!"); break;
-  case 3 : Console.WriteLine("Это день недели Среда!"); break;
-  case 4 : Console.WriteLine("Это день недели Четверг!"); break;
-  case 5 : Console.WriteLine("Это день недели Пятница!"); break;
-  case 6 : Console.WriteLine("Это день недели Суббота!"); break;
-  case 7 : Console.WriteLine("Это день недели Воскресенье!"); break;
-}
+            
+      switch(a1)
+        {
+          case 1 : Console.WriteLine("Это день недели Понедельник!"); break;
+          case 2 : Console.WriteLine("Это день недели Вторник!"); break;
+          case 3 : Console.WriteLine("Это день недели Среда!"); break;
+          case 4 : Console.WriteLine("Это день недели Четверг!"); break;
+          case 5 : Console.WriteLine("Это день недели Пятница!"); break;
+          case 6 : Console.WriteLine("Это день недели Суббота!"); break;
+          case 7 : Console.WriteLine("Это день недели Воскресенье!"); break;
+        }
 
-if (a1 < 1 ^ a1 > 7) 
-{
-  Console.WriteLine("Такого дня недели нету!");
-}
-else
-{
-  if (a1 == 6 ^ a1 == 7) 
-  {Console.WriteLine("Это выходной день!");}
-  else
-  {Console.WriteLine("Это не выходной день!");}*/
+      if (a1 < 1 ^ a1 > 7) 
+        {
+          Console.WriteLine("Такого дня недели нету!");
+        }
+      else
+        {
+          if (a1 == 6 ^ a1 == 7) 
+            {Console.WriteLine("Это выходной день!");}
+          else
+            {Console.WriteLine("Это не выходной день!");}
+        }
+
+    }
 
 
+
+//Конец тела задач    
   Console.Write("Нажмите <Enter> для продолжения... ");
       while (Console.ReadKey().Key != ConsoleKey.Enter) {}
 
@@ -94,10 +97,11 @@ else
     Console.WriteLine("Вы хотите воспользоваться еще одним решением? Yes/No");
     Console.WriteLine(" ");
     otvet2 = Console.ReadLine();
+
     if (otvet2 != "No" && otvet2 != "no" && otvet2 != "NO" && otvet2 !="n" && otvet2 !="N" 
         && otvet2 != "Yes" && otvet2 != "yes" && otvet2 != "YES" && otvet2 !="y" && otvet2 !="Y")
     {
-    Console.WriteLine("Не коректный ответ");
+    Console.WriteLine("Некоректный ответ");
     Console.Write(" Нажмите <Enter> для повторго ввода... ");
       while (Console.ReadKey().Key != ConsoleKey.Enter) {}
     }
